86b417107e3672355b5f790f80eca7cd1dcde011
Ivan Andrianov <ivanushka2003@gmail.com>
2026-02-23 12:51:01 +0300

    1. Init коммит.

---
+++ src/test/java/ru/guu/eventflow/EventFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.guu.eventflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
